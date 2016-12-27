8f1dd2f6a9ac75329c0af7805908b2d33e860ad2
Abdullah Çetinkaya <mail@abdullahcetinkaya.com.tr>
2016-12-27 09:17:01 +0300

    upload

--- include/sidebar/menu.php
+++ include/sidebar/menu.php
@@ -4,7 +4,8 @@
             <li><a href="<?php echo $_link.'form.php';?>"><i class="fa fa-edit"></i> Add Domain </a></li>
 			<li><a href="<?php echo $_link.'table.php';?>"><i class="fa fa-table"></i> Domain List </a></li>
 			<li><a href="<?php echo $_link.'settings.php';?>"><i class="fa fa-cog"></i> Settings </a></li>
-			
+
+
 	</ul>
 
 </div> 
