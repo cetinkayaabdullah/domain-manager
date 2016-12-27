3e68f39892266153684ee0ba242749cd60eeea39
Abdullah Çetinkaya <mail@abdullahcetinkaya.com.tr>
2016-12-27 09:19:19 +0300

    upload

--- include/sidebar/menu.php
+++ include/sidebar/menu.php
@@ -1,11 +1,8 @@
 <div class="menu_section">
-    
     <ul class="nav side-menu">
             <li><a href="<?php echo $_link.'form.php';?>"><i class="fa fa-edit"></i> Add Domain </a></li>
 			<li><a href="<?php echo $_link.'table.php';?>"><i class="fa fa-table"></i> Domain List </a></li>
 			<li><a href="<?php echo $_link.'settings.php';?>"><i class="fa fa-cog"></i> Settings </a></li>
-
-
+            <li><a href="<?php echo $_link.'cron/autoupload.php';?>"><i class="fa fa-cog"></i> Auto Check </a></li>
 	</ul>
-
 </div> 
